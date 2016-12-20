0fcb93c087057a0946ba1253dca843fbdcb78241
cag2050 <cag2050@163.com>
2016-12-20 16:51:32 +0800

    m

--- index.test.js
+++ index.test.js
@@ -6,20 +6,18 @@ var expect = require('chai').expect;
 var fork = require('child_process').fork;
 
 describe('test index.html', function () {
-    //var child;
-    //before(function (done) {
-    //    child = fork('./server.js');
-    //    child.on('message', function (msg) {
-    //        if (msg === 'listening') {
-    //            done();
-    //        }
-    //    })
-    //});
-    //after(function () {
-    //    child.kill();
-    //})
-    it('点击后标题改变', function(done) {
-        var nightmare = Nightmare({ show: true });
+    // 用server.js运行子进程,启动http服务器
+    var child;
+    before(function (done) {
+        child = fork('./server.js');
+        done(); // done是 Mocha 提供的一个函数，用来表示异步操作完成。如果不调用done，Mocha 就会认为异步操作没有结束，一直停在这一步，不往下执行，从而导致超时错误。
+    });
+    after(function () {
+        child.kill();
+    });
+
+    it('点击后标题改变', function (done) {
+        var nightmare = Nightmare({show: true});
         nightmare
             .goto('http://127.0.0.1:8080/index.html')
             .click('h1')
@@ -28,7 +26,7 @@ describe('test index.html', function () {
                 return document.querySelector('h1').textContent;
             })
             .end()
-            .then(function(text) {
+            .then(function (text) {
                 expect(text).to.equal('Hello Clicked');
                 done();
             })
--- server.js
+++ server.js
@@ -4,4 +4,3 @@
 var httpServer = require('http-server');
 var server = httpServer.createServer();
 server.listen(8080);
-//process.send('listening');
